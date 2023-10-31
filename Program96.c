6eab2894948e2e9f0d7c6ac8937522a25c267d8a
Borse-Shubham <shubhamborse0911@gmail.com>
2023-10-31 22:56:02 +0530

    LB Programs on Pattern Printing

---
+++ Program96.c
@@ -0,0 +1,40 @@
+
+#include<stdio.h>
+
+void Display(int iRow, int iColumn)
+{
+    int i = 0, j = 0;
+    
+    for(i = 1; i <= iRow; i++)
+    {
+        for(j = 1; j <= iColumn; j++)
+        {
+            if(i == j)
+            {
+                printf("#\t");
+            }
+            else if(i > j)
+            {
+                printf("*\t");
+            }
+            
+        }
+        printf("\n\n");
+    }
+    
+}
+
+int main()
+{
+    int iNo1 = 0, iNo2 = 0;
+
+    printf("Enter the number of rows : \n");
+    scanf("%d",&iNo1);
+
+    printf("Enter the number of Columns : \n");
+    scanf("%d",&iNo2);
+
+    Display(iNo1, iNo2);
+
+    return 0;
+}
